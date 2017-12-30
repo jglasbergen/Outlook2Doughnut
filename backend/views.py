aa94b9222516a7cbc30c474db5879943a6c6639e
Jaap Glasbergen <j.glasbergen@outlook.com>
2017-12-30 14:14:04 +0100

    Aanpassingen tbv start van integratie django en polymer

--- backend/views.py
+++ backend/views.py
@@ -5,4 +5,10 @@ from django.views import generic
 # Create your views here.
 class IndexView(generic.base.TemplateView):
     
-    template_name = 'index.html'
+    # template_name = 'ui/index.html'
+    template_name = 'index-source.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['csv_data'] = "CSV_Data"
+        return context    
