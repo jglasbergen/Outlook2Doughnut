6fdae20d68d5f53e408f7e07e6569212345a117e
Jaap Glasbergen <Jaap.Glasbergen@T_MC.nl>
2018-01-02 15:39:37 +0100

    Updated .gitignore

--- project/settings.py
+++ project/settings.py
@@ -56,6 +56,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'frontend')],
         'DIRS': [os.path.join(BASE_DIR, 'backend/static')],
         'APP_DIRS': True,
         'OPTIONS': {
